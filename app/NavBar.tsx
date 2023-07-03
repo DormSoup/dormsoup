b3a326552feffeab3969829bcdd8628476b9b219
Chengyuan Ma <macy404@mit.edu>
2023-07-02 20:56:13 -0400

    Small tweaks on navbar sizing
    The logo size finally stops changing

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -25,13 +25,13 @@ export default function NavBar() {
   };
 
   return (
-    <div className="fixed top-0 flex h-fit w-full flex-row items-center gap-1 border-b-2 border-gray-300 bg-white px-4 py-2 text-black z-30">
-      <Link className="w-1/4 flex-none md:w-1/6 lg:w-1/12" href="/">
-        <div className="relative h-8 w-full">
-          <Image src="/logo.svg" alt="DormSoup Logo" fill style={{ objectFit: "contain" }} />
+    <div className="fixed top-0 z-30 flex h-14 w-full flex-row items-center gap-1 border-b-2 border-gray-300 bg-white px-4 py-2 text-black">
+      <Link className="w-32 flex-none" href="/">
+        <div className="relative h-8 w-32">
+          <Image src="/logo.svg" alt="DormSoup Logo" fill className="object-contain" />
         </div>
       </Link>
-      <div className="relative mx-4 grow transition max-sm:focus-within:fixed max-sm:focus-within:w-[calc(100vw-3rem)] max-sm:focus-within:mx-auto">
+      <div className="relative mx-4 grow max-sm:focus-within:fixed max-sm:focus-within:mx-auto max-sm:focus-within:w-[calc(100%-2rem)]">
         <input
           className="w-full rounded-xl border-2 border-gray-300 bg-gray-50 pl-6 pr-4"
           placeholder="Search here"
