5ec99ac46c6aecd40a85e7584b7459f25a1a7f35
Andi Liu <andiliu@mit.edu>
2023-07-14 20:39:25 -0400

    Remove Other, Music, HASS, FSILG tags

--- app/components/EventTagsBar.tsx
+++ app/components/EventTagsBar.tsx
@@ -10,6 +10,8 @@ import styles from "./EventTagsBar.module.css";
 
 type TagDisplayConfig = { color: string; icon: string };
 
+// TODO: Add Sale, Party, Fiance=Quant,
+
 const eventTagDisplayConfigs: { [tagName: string]: TagDisplayConfig } = {
   Theater: { color: "#824C71", icon: "\u{f630}" },
   Talk: { color: "#242F40", icon: "\u{f8cb}" },
@@ -21,18 +23,18 @@ const eventTagDisplayConfigs: { [tagName: string]: TagDisplayConfig } = {
   // Workshop: { color: "#5B3000", icon: "\u{f7d9}" },
   Rally: { color: "#A61C3C", icon: "\u{f757}" },
   "Class Presentation": { color: "#F7B801", icon: "\u{e3aa}" },
-  Other: { color: "#A97C73", icon: "\u{003f}" },
+  // Other: { color: "#A97C73", icon: "\u{003f}" },
   EECS: { color: "#FF8811", icon: "\u{f2db}" },
   AI: { color: "#3C91E6", icon: "\u{e0c6}" },
   Math: { color: "#342E37", icon: "\u{f68b}" },
   Biology: { color: "#1C5D99", icon: "\u{f471}" },
-  Music: { color: "#C2E812", icon: "\u{f001}" },
-  Concert: { color: "#F78154", icon: "\u{f8db}" },
+  Concert: { color: "#C2E812", icon: "\u{f001}" },
+  // Concert: { color: "#F78154", icon: "\u{f8db}" },
   "Quant/Finance": { color: "#00A676", icon: "\u{0024}" },
   "East Asian": { color: "#FFA62B", icon: "\u{e3f7}" },
-  FSILG: { color: "#F76F8E", icon: "\u{f7ae}" },
+  // FSILG: { color: "#F76F8E", icon: "\u{f7ae}" },
   Religion: { color: "#6A6262", icon: "\u{f684}" },
-  HASS: { color: "#615055", icon: "\u{f19c}" },
+  // HASS: { color: "#615055", icon: "\u{f19c}" },
   Dance: { color: "#F25F5C", icon: "D" },
   Entrepreneurship: { color: "#0B5563", icon: "\u{f2b5}" },
   "Free Food": { color: "#E03616", icon: "\u{f818}" },
