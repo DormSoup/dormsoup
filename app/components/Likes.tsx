6b9340bdc2c786517e2b18ad979959c4ad2b2792
Andi Liu <andiliu@mit.edu>
2023-08-05 10:46:12 -0400

    Clicking on number of likes can like an event

--- app/components/Likes.tsx
+++ app/components/Likes.tsx
@@ -11,14 +11,14 @@ import { useAppDispatch } from "../redux/store";
 export default function Likes({ event }: { event: SerializableEvent }) {
   const dispatch = useAppDispatch();
   return (
-    <div className="flex flex-col items-center justify-center whitespace-nowrap pl-2 pt-1 text-red-500 transition-all duration-150 hover:scale-125">
-      <FontAwesomeIcon
-        icon={event.liked ? faHeartSolid : faHeart}
-        onClick={(clickEvent) => {
-          dispatch(likeEvent(event.id));
-          clickEvent.stopPropagation();
-        }}
-      />
+    <div
+      className="flex flex-col items-center justify-center whitespace-nowrap pl-2 pt-1 text-red-500 transition-all duration-150 hover:scale-125"
+      onClick={(clickEvent) => {
+        dispatch(likeEvent(event.id));
+        clickEvent.stopPropagation();
+      }}
+    >
+      <FontAwesomeIcon icon={event.liked ? faHeartSolid : faHeart} />
       <div className={event.likes < 10 ? "text-md" : "text-sm"}> {event.likes} </div>
     </div>
   );
