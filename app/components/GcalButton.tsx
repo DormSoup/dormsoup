ac29c4eea9c3a85c262a5daea3b77716ff37c429
t11s <greenfeatherhelp@gmail.com>
2024-11-05 21:21:49 -0500

    feat: transition opacity when hovering gcal button

--- app/components/GcalButton.tsx
+++ app/components/GcalButton.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function GcalButton() {
   return (
     <a
-      className="flex w-full flex-row items-center justify-center rounded-md bg-white px-4 py-1 text-lg font-semibold md:w-auto"
+      className="flex w-full flex-row items-center justify-center rounded-md bg-white px-4 py-1 text-lg font-semibold transition-opacity hover:opacity-75 md:w-auto"
       target="_blank"
       href="https://calendar.google.com/calendar/u/0?cid=Y18zNjE2MDRhYThlZGRjNGJlMTA0NmI0ZmJmODNhYmZkYjExMzE5NWNhZmIxYTI3NWM4YWZmN2ZlMDgwZmIyNDEzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20"
     >
