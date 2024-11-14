0fddb1effe89b6c9d488ccc8e46e6270818b6585
Dana <danayim@bu.edu>
2024-11-14 02:30:30 -0500

    Added styling for comment section, rearranged the comment's components, and ensured that each comment has a unique comment id.

--- app/components/Comments.tsx
+++ app/components/Comments.tsx
@@ -29,7 +29,7 @@ import { Session } from "../api/auth/session/route";
 // Importing GetEventDetailResponse for event detail typing
 import { GetEventDetailResponse } from "../api/event-detail/route";
 
-export default function Comments({ event }: { event: SerializableEvent;}) {
+export default function Comments({ event }: { event: SerializableEvent; }) {
     // setting up state for the session and eventDetail
     const [session, setSession] = useState<Session | undefined>(undefined);
     const [eventDetail, setEventDetail] = useState<GetEventDetailResponse | undefined>(undefined);
@@ -46,20 +46,22 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
     // Fetch eventDetail data
     useEffect(() => {
         if (event) {
-        fetch(`/api/event-detail?id=${event.id}`)
-            .then((response) => response.json())
-            .then((data: GetEventDetailResponse) => setEventDetail(data))
-            .catch((error) => console.error("Failed to fetch event detail:", error));
+            fetch(`/api/event-detail?id=${event.id}`)
+                .then((response) => response.json())
+                .then((data: GetEventDetailResponse) => setEventDetail(data))
+                .catch((error) => console.error("Failed to fetch event detail:", error));
         }
     }, [event]);
 
     // state variables for comments, input value, likes, and liked status
-    const [comments, setComments] = useState<{ id: number; userName: string; text: string; replies: { userName: string; text: string }[] }[]>([]);
+    const [comments, setComments] = useState<
+        { id: number; userName: string; text: string; replies: { id: number; userName: string; text: string }[] }[]
+    >([]);
     const [inputValue, setInputValue] = useState("");
     const [likes, setLikes] = useState(event.likes);
     const [liked, setLiked] = useState(event.liked);
-    const [replyTo, setReplyTo] = useState<string | null>(null);
     const [replyToCommentId, setReplyToCommentId] = useState<number | null>(null);
+    const [replyToUserName, setReplyToUserName] = useState<string | null>(null);
 
     // Function to handle text input changes and adjust textarea height
     const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -74,23 +76,36 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
     // Function to handle keydown event
     const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
         if (e.key === "Enter" && !e.shiftKey) {
-        e.preventDefault(); // Prevent the default behavior of Enter key (i.e., adding a new line)
-        handlePost(); // Call the handlePost function
+            e.preventDefault(); // Prevent the default behavior of Enter key (i.e., adding a new line)
+            handlePost(); // Call the handlePost function
         }
+    };
+
+    const handleReplyClick = (commentId: number, userName: string) => {
+        console.log("replying to comment id:", commentId);
+        setReplyToCommentId(commentId);
+        setReplyToUserName(userName);
+        setInputValue(`@${userName} `);
+        textareaRef.current?.focus(); // Focus on the input box
     };
 
     const handlePost = () => {
         if (inputValue.trim() === "") return;
-    
+
         if (replyToCommentId !== null) {
             // Handle posting a reply to a specific comment
             const newComments = [...comments];
-            const commentIndex = newComments.findIndex(comment => comment.id === replyToCommentId);
-            if (commentIndex !== -1) {
-                newComments[commentIndex].replies.push({ userName: session?.user?.name || "Anonymous", text: inputValue });
+            const parentCommentIndex = newComments.findIndex(comment => comment.id === replyToCommentId);
+            if (parentCommentIndex !== -1) {
+                newComments[parentCommentIndex].replies.push({
+                    id: Date.now(), // Generate a unique ID for the reply
+                    userName: session?.user?.name || "Anonymous",
+                    text: inputValue
+                });
                 setComments(newComments);
             }
             setReplyToCommentId(null); // Clear the reply state
+            setReplyToUserName(null);
         } else {
             // Handle posting a regular comment
             const newComment = {
@@ -101,18 +116,11 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
             };
             setComments([...comments, newComment]);
         }
-    
+
         setInputValue("");
         if (textareaRef.current != null) {
             textareaRef.current.style.height = "auto";
         }
-    };
-    
-
-    const handleReplyClick = (commentId: number, userName: string) => {
-        setReplyToCommentId(commentId);
-        setInputValue(`@${userName} `);
-        textareaRef.current?.focus(); // Focus on the input box
     };
 
     // function to handle "add to calendar"
@@ -169,33 +177,50 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
         <div className="flex flex-col h-full">
 
             {/* Comment Section */}
-            <div className="flex-1 px-4 py-2">
-            {comments.length === 0 ? (
-                <div className="text-slate-400">No comments</div>
-            ) : (
-                comments.map((comment) => (
-                <div key={comment.id} className="my-2 break-words mr-2">
-                    <span className="text-sm">{comment.userName}</span> <br />
-                    {comment.text}
-                    <div>
-                        <button onClick={() => handleReplyClick(comment.id, comment.userName)} className="mt-1 text-sm text-slate-400 hover:text-black">
-                            Reply
-                        </button>
-                    </div>
-                        
-                    {/* Reply Section */}
-                    <div className="ml-4 mt-2">
-                        {comment.replies.map((reply, replyIndex) => (
-                            <div key={replyIndex} className="my-1 text-sm">
-                                <span>{reply.userName}</span>: {reply.text}
+            <div className="flex-1 px-4 py-4 space-y-4">
+                {comments.length === 0 ? (
+                    <div className="text-slate-400">No comments</div>
+                ) : (
+                    comments.map((comment) => (
+                        <div key={comment.id} className="my-2 break-words mr-2">
+                            <span className="text-sm text-white bg-logo-yellow border border-logo-yellow rounded-full px-3 py-1 mr-2">
+                                {comment.userName}
+                            </span>
+                            {comment.text}
+                            <div>
+                                <button
+                                    onClick={() => handleReplyClick(comment.id, comment.userName)}
+                                    className="ml-2 mt-1 text-sm text-slate-400 hover:text-black"
+                                >
+                                    Reply
+                                </button>
                             </div>
-                        ))}
-                    </div>
-                </div>
-            ))
-        )}
-        </div>
-        
+
+                            {/* Reply Section */}
+                            <div className="ml-6 mt-2 space-y-2">
+                                {comment.replies.map((reply) => (
+                                    <div key={reply.id} className="space-y-1">
+                                        <span className="text-sm text-white bg-logo-red border border-logo-red rounded-full px-3 py-1 mr-2">
+                                            {reply.userName}
+                                        </span>
+                                        {reply.text}
+                                        <div>
+                                            <button
+                                                onClick={() => handleReplyClick(reply.id, reply.userName)}
+                                                className="ml-2 mt-1 text-sm text-slate-400 hover:text-black"
+                                            >
+                                                Reply
+                                            </button>
+                                        </div>
+                                        
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
+
             {/* Likes and Calendar Buttons */}
             <div className="flex items-center px-4 py-2 border-t border-gray-300">
                 <div onClick={onLikeButtonClicked} className="cursor-pointer mr-4">
@@ -207,20 +232,20 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
             </div>
 
             <div className="ml-4 text-sm mb-4">
-                    {likes} {likes === 1 ? "like" : "likes"}
+                {likes} {likes === 1 ? "like" : "likes"}
             </div>
-        
+
             {/* Comment Input Section */}
             <div className="mb-4 ml-4">
                 <div className="flex flex-row items-center">
                     <textarea
-                    ref={textareaRef}
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Add a comment..."
-                    className="flex-1 border rounded-lg p-2 resize-none overflow-hidden"
-                    rows={1}
+                        ref={textareaRef}
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onKeyDown={handleKeyDown}
+                        placeholder="Add a comment..."
+                        className="flex-1 border rounded-lg p-2 resize-none overflow-hidden"
+                        rows={1}
                     />
                     <button onClick={handlePost} className="mx-2 p-2 text-slate-400 hover:text-black rounded">
                         Post
@@ -229,5 +254,4 @@ export default function Comments({ event }: { event: SerializableEvent;}) {
             </div>
         </div>
     );
-        
 }
